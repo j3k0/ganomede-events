066e145f8fedb68acdca18cdccd888eeaae0a8af
Alexey Zabrodsky <elmigranto@gmail.com>
2017-02-14 06:18:02 +0500

    Implement Client with ability to listen for events.

---
+++ src/client/Client.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const url = require('url');
+const {EventEmitter} = require('events');
+const lodash = require('lodash');
+const requestEvents = require('./request-events');
+const config = require('../../config');
+
+// Some events are special, and have nothing to do with our channles,
+// we should not start HTTP request for them. should not monitor them:
+//
+const ignoreChannels = [
+  'newListener',    // EventEmitter's after listener added
+  'removeListener', // EventEmitter's after listener removed
+  'error',          // HTTP request errored
+  'drain'           // All the requests are finished and there are no listeners left.
+];
+
+class Client extends EventEmitter {
+  constructor (id, {
+    secret,
+    agent,
+    protocol = 'http',
+    hostname = 'localhost',
+    port = 3000,
+    pathname = `${config.http.prefix}/events`
+  } = {}) {
+    const normalizedProtocol = protocol.endsWith(':') ? protocol : `${protocol}:`;
+
+    super();
+    this.id = id;
+    this.polls = {}; // channel => Boolean ()
+    this.agent = agent || require(normalizedProtocol.slice(0, -1)).globalAgent;
+    this.apiRoot = url.format({
+      protocol: normalizedProtocol,
+      hostname,
+      port,
+      pathname
+    });
+  }
+
+  emitEvent (event) {
+    const service = event.from;
+    const eventType = event.type;
+    this.emit(service, event);
+    this.emit(`${service}:${eventType}`, event);
+  }
+
+  request (channel, callback) {
+    const options = {
+      uri: this.apiRoot,
+      method: 'get',
+      agent: this.agent,
+      json: true,
+      gzip: true,
+      qs: {
+        secret: this.secret,
+        clientID: this.id,
+        channel
+      }
+    };
+
+    requestEvents(options, callback);
+  }
+
+  checkForDrain () {
+    if (lodash.values(this.polls).every(status => !status))
+      this.emit('drain');
+  }
+
+  startPolling (channel) {
+    if (this.polls[channel])
+      return;
+
+    this.polls[channel] = true;
+
+    this.request(channel, (err, events) => {
+      if (err)
+        this.emit('error', channel, err);
+      else
+        events.forEach(e => this.emitEvent(e));
+
+      this.polls[channel] = false;
+
+      // If there are still listeners for this channel, keep polling.
+      // Otherewise we might be in a situation when all the requests
+      // are finished, appropriated events are emitted, and no one listening.
+      if (this.listenerCount(channel) === 0)
+        return this.checkForDrain();
+
+      process.nextTick(() => this.startPolling(channel));
+    });
+  }
+
+  on (channel, handler) {
+    // TODO
+    // perhaps print warnings of some kind
+    if (!ignoreChannels.includes(channel))
+      this.startPolling(channel);
+
+    super.on(channel, handler);
+  }
+}
+
+module.exports = Client;
---
+++ tests/Client/Client.test.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const url = require('url');
+const config = require('../../config');
+
+describe.only('Client', () => {
+  let Client;
+  let requestEvents;
+
+  beforeEach(() => {
+    requestEvents = td.replace('../../src/client/request-events');
+    Client = require('../../src/client/Client');
+  });
+
+  const apiRoot = 'http://localhost:3000/events/v1';
+  const createClient = () => new Client('clientId', Object.assign(
+    {secret: config.secret},
+    url.parse(apiRoot)
+  ));
+
+  it('keeps reqeusting new messages', (done) => {
+    const client = createClient();
+    let nEmits = 0;
+    let nCalls = 0;
+
+    td.when(requestEvents(td.matchers.isA(Object), td.matchers.isA(Function)))
+      .thenDo((opts, cb) => {
+        ++nCalls;
+        setImmediate(cb, null, [
+          {from: 'service', type: 'type'},
+          {from: 'service', type: 'type'},
+        ]);
+      });
+
+    const handler = event => {
+      ++nEmits;
+      if (nEmits === 4) {
+        client.removeListener('service', handler);
+        client.removeListener('service:type', handler);
+        expect(nCalls).to.equal(2);
+      }
+      else if (nEmits > 4)
+        done(new Error('oops too many emits ' + nEmits));
+    };
+
+    client.on('service:type', handler);
+    client.on('service', handler);
+    client.on('drain', done);
+  });
+
+  it('#once() works without rescheduling', (done) => {
+    const client = createClient();
+    const expectedEvent = {from: 'ch', type: 'type'};
+    let nEmits = 0;
+    let nCalls = 0;
+
+    td.when(requestEvents(td.matchers.isA(Object), td.matchers.isA(Function)))
+      .thenDo((opts, cb) => {
+        ++nCalls;
+        setImmediate(cb, null, [
+          expectedEvent,
+          {from: 'ch', type: 'second event that will get "ignored"'}
+        ]);
+      });
+
+    // this gets called first
+    client.once('ch', (event) => {
+      ++nEmits;
+      expect(event).to.equal(expectedEvent);
+      expect(nCalls).to.equal(1);
+      expect(nEmits).to.equal(1);
+    });
+
+    client.once('ch', (event) => {
+      ++nEmits;
+      expect(event).to.equal(expectedEvent);
+    });
+
+    client.once('drain', (event) => {
+      expect(nCalls).to.equal(1);
+      expect(nEmits).to.equal(2);
+      done();
+    });
+  });
+
+  it('subscribing multiple times does not trigger multiple simultaneous requests', (done) => {
+    const client = createClient();
+    const calls = [];
+    let nEmits = 0;
+
+    td.when(requestEvents(td.matchers.isA(Object), td.matchers.isA(Function)))
+      .thenDo((opts, cb) => {
+        calls.push(opts.qs.channel);
+        setImmediate(cb, null, [
+          {from: 'ch', type: 'type'},
+          {from: 'ch2', type: 'type'}
+        ]);
+      });
+
+    const handler = (channel) => {
+      const myself = (event) => {
+        ++nEmits;
+        client.removeListener(channel, myself);
+      };
+
+      return myself;
+    };
+
+    client.on('ch', handler('ch'));                 // 1 (emit because removes after call)
+    client.on('ch:type', handler('ch:type'));       // 1 (emit because removes after call)
+    client.once('ch2', () => ++nEmits);             // 1
+    client.once('ch2:type', () => ++nEmits);        // 1
+
+    client.on('drain', () => {
+      expect(nEmits).to.equal(4);
+      expect(calls).to.eql(['ch', 'ch:type', 'ch2', 'ch2:type']);
+      done();
+    });
+  });
+});

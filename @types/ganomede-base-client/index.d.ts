c396f158ca5bd110513ebfe36a7f349528a804c2
Hussein Taha(Ir0nDev) <taha.hussein@outlook.com>
2021-11-29 12:13:55 +0000

    adding the comment line of generating the type definitions

--- @types/ganomede-base-client/index.d.ts
+++ @types/ganomede-base-client/index.d.ts
@@ -1,49 +1,50 @@
-import {JsonClient} from 'restify-clients'
+// generating this file by using the command: npx -p typescript tsc src/**/*.js --declaration --allowJs --emitDeclarationOnly --outDir types
+import { JsonClient } from 'restify-clients'
 export = BaseClient;
 declare class BaseClient {
-    static parseConstructorOptions(baseUrl: any, optionsOverwrites: any): {
-        pathPrefix: string;
-        apiOptions: any;
-    };
-    constructor(baseUrl: any, optionsOverwrites?: {});
+  static parseConstructorOptions(baseUrl: any, optionsOverwrites: any): {
     pathPrefix: string;
-    api: JsonClient;
-    _checkArgs({ method, path, headers, body, qs }: {
-        method: any;
-        path: any;
-        headers: any;
-        body: any;
-        qs: any;
-    }): void;
-    _extendedHeaders({ body, qs }: {
-        body: any;
-        qs: any;
-    }): {
-        'x-request-id': any;
-    } | {
-        'x-request-id'?: undefined;
-    };
-    apiCall({ method, path, headers, body, qs }: {
-        method: any;
-        path: any;
-        headers?: {};
-        body?: any;
-        qs?: any;
-    }, callback: any): any;
+    apiOptions: any;
+  };
+  constructor(baseUrl: any, optionsOverwrites?: {});
+  pathPrefix: string;
+  api: JsonClient;
+  _checkArgs({ method, path, headers, body, qs }: {
+    method: any;
+    path: any;
+    headers: any;
+    body: any;
+    qs: any;
+  }): void;
+  _extendedHeaders({ body, qs }: {
+    body: any;
+    qs: any;
+  }): {
+    'x-request-id': any;
+  } | {
+    'x-request-id'?: undefined;
+  };
+  apiCall({ method, path, headers, body, qs }: {
+    method: any;
+    path: any;
+    headers?: {};
+    body?: any;
+    qs?: any;
+  }, callback: any): any;
 }
 declare namespace BaseClient {
-    export namespace defaultOptions {
-        namespace retry {
-            const minTimeout: any;
-            const maxTimeout: any;
-            const retries: number;
-        }
-        const headers: {
-            accept: string;
-            'accept-encoding': string;
-        };
+  export namespace defaultOptions {
+    namespace retry {
+      const minTimeout: any;
+      const maxTimeout: any;
+      const retries: number;
     }
-    export { RequestSpecError };
+    const headers: {
+      accept: string;
+      'accept-encoding': string;
+    };
+  }
+  export { RequestSpecError };
 }
 declare class RequestSpecError {
 }
